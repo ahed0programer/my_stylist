c9284db079316837b5418d77430a6ca9609e1bb9
ahed0programer@ <ahedsuleiman6@gmail.com>
2024-06-09 11:24:16 +0300

    Ahed added a prototype of what suggest_style method should be like

--- stylist_KB.py
+++ stylist_KB.py
@@ -1,4 +1,13 @@
+
 from experta import *
+import pandas as pd 
+
+matched_colors = pd.read_csv("matched_color.csv")
+clothes = pd.read_csv("clothes.csv")
+
+class Color_Matched(Fact):
+    pass
+
 
 class Cloth(Fact):
     name = Field(str , True)
@@ -6,11 +15,13 @@ class Cloth(Fact):
     category = Field(str , True)
     size = Field(any , True)
 
+# * A class express the clothes the user has 
 class My_closet(Fact):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     cloth_id = Field(str , True)
 
+# a class express the occasion the user want to attend if any 
 class Occasion(Fact):
     pass
 
@@ -19,19 +30,33 @@ class MyStylist_Engine(KnowledgeEngine):
         super().__init__()
 
     @DefFacts()
-    def init_Data():
-        yield Cloth(name= "xhoof shirt" , color="red" , category="men/shirts" ,size ="large");
-        yield Cloth(name= "xhoof shirt" , color="gray" , category="men/shirts" ,size ="Medium");
-        
+    def init_Data(self):
+        # creating Facts from data stored as a CSV files
+        # * import clothes
+        for i in clothes.index:
+            yield Cloth(name= clothes.loc[i , "name"] ,
+                        color=clothes.loc[i , "color"]  ,
+                        category=clothes.loc[i , "category"],
+                        size =clothes.loc[i , "size"] );
+    
+        # * import Colors match each other
+        for i in matched_colors.index:
+            yield Color_Matched(matched_colors.loc[i , "color1"] , matched_colors.loc[i , "color2"] , matched = matched_colors.loc[i , "match"] )
+             
 
+    @Rule(  Cloth(name=MATCH.name1 ,color=MATCH.color1  , category=W(),size=W()),
+            Cloth(name=MATCH.name2 ,color=MATCH.color2  , category=W(),size=W()),
+            Color_Matched(MATCH.color1 , MATCH.color2 , matched =True )
+          )
+    def suggest_style(self , name1  ,color1, name2 , color2):
+        print(f"you can wear :{color1} {name1} with {color2} {name2} ")
 
 
     @Rule()
-    def suggest_style():
+    def suggest_clothToBuy(self):
         pass
 
-    @Rule()
-    def suggest_clothToBuy():
-        pass
-
-  
+myStylistEngine = MyStylist_Engine()
+myStylistEngine.reset()
+myStylistEngine.run()
+# print(myStylistEngine.facts)
